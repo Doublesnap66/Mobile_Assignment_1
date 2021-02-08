1a33f58ec4388f1e4d71d60492c45a4a4cf88206
Todge <todgie16@gmail.com>
2021-02-08 01:59:47 -0500

    Added the textview and button with an on click event

--- Lab2/app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ Lab2/app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -2,13 +2,35 @@ package edu.temple.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.helloworld);
+
+        View.OnClickListener onClickListener = new ButtonClickListener();
+
+        button.setOnClickListener(onClickListener);
+    }
+
+    private class ButtonClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            textView.setText("You clicked the button");
+        }
     }
 }
